455d42d67cc5748ea6e8c6a8dd478b503da688ba
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-06-05 10:32:14 +0700

    Add files via upload

---
+++ homework2.ru.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+course = 'Python'
+one_task = hours/homework
+print('Курс:', course,',всего задачь:',homework,',затрачено часов:',hours,',среднее время выполнения',one_task,'.')
